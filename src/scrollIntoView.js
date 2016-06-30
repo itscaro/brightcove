c2fd8e9b204148fb72d4485145fe84f4ebe419f4
Minh-Quan TRAN <mq.tran@prismamedia.com>
2016-06-30 14:23:42 +0000

    scope

--- src/scrollIntoView.js
+++ src/scrollIntoView.js
@@ -1,21 +1,26 @@
 /**
  * Scroll Into View
  */
-videojs.plugin('scrollIntoView', function() {
-    var myPlayer = this;
+(function () {
+    var scrollIntoViewFunc = function () {
+        var player = this;
 
-    var checkIfVideoInView = function() {
-        // Player is fully in viewport, is never played and is in pause
-        if ( $(myPlayer.el_).isOnScreen(1, 1).inView && !myPlayer.hasStarted_ && myPlayer.paused()) {
-            myPlayer.play()
+        var checkIfVideoInView = function () {
+            // Player is fully in viewport, is never played and is in pause
+            if ($(player.el_).isOnScreen(1, 1).inView && !player.hasStarted_ && player.paused()) {
+                player.play()
+            }
         }
+
+        var timer;
+        $(window).on('scroll.playOnScroll', function () {
+            // Trigger 250ms after when finish scrolling and only once
+            clearTimeout(timer)
+            timer = setTimeout(function () {
+                checkIfVideoInView()
+            }, 250)
+        });
     }
 
-    var timer;
-    $(window).on('scroll.playOnScroll', function () {
-        // Trigger 250ms after when finish scrolling and only once
-        clearTimeout(timer)
-        timer = setTimeout(function() {checkIfVideoInView()}, 250)
-    });
-});
-
+    videojs.plugin('scrollIntoView', scrollIntoViewFunc);
+}();

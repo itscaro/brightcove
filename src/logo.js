5eae60b7f24cfa06f60d29394d0153d63f363499
Minh-Quan TRAN <mq.tran@prismamedia.com>
2016-06-30 16:31:20 +0200

    update

--- src/logo.js
+++ src/logo.js
@@ -1,30 +1,33 @@
 /**
  * Logo
  */
-(function(videojs) {
-    var logoFunc = function(options) {
-        console.debug('Plugin logo', options);
-        
-        // Create variables and new div, anchor and image for company logo
-        var player = this,
-            title,
-            newElement = document.createElement('div'),
-            newImage = document.createElement('img');
+(function (videojs) {
+  var logoFunc = function (options) {
+    defaultOptions = {}
+    options = options || {}
+    options = videojs.mergeOptions(defaultOptions, options)
+    console.debug('Plugin logo', options)
 
-        // Assign id and classes to div for logo
-        newElement.classList.add('vjs-logo');
-        newElement.style.float = 'left';
+    // Create variables and new div, anchor and image for company logo
+    var player = this,
+      title,
+      newElement = document.createElement('div'),
+      newImage = document.createElement('img');
 
-        // Assign properties to elements and assign to parents
-        newImage.setAttribute('src', 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAoCAYAAACM/rhtAAACh0lEQVRYCdVYy24UMRCsJiCRIOAESIggEK8LX8ONH+CQMwd+gz/gygV+J4E8pChw4IIiJARIpFBH66Vn1j1Ts0xmg6WVPba7XS5313gH+J8KybdcXXld4+pCq/NJ63nKx+3aYnOAJA3Ao9qkifp2auvMAQLYBLBRmzRRXzeDAB5PBKS2zDcz+1wbiAyuMv4+1MB533kBWD3e8wSwmiBtgKuMwZRBl5bTQvJZ68jL0BsAV8pDUh8AeAngN4CTWe3t8qv1xbkHZvY98Z13k7wqvlje517+beRij7l67LvFD0nX0pvluaM+NrOvHeP9QySfiwy+KN5Ibok2r4pNVx1lpjZvMIMDBD/VvghkLIAfg1P1fZ5KS/DVEOrYX9oKgz8AHBUDkUHP6r1gkzb7GFTY2DMz+gokLwG4l672d+DQzHxjvSUFSNIz8XqvB2CewQDuA+hTBncpxZ9PTAGKR+U+YvwpIeE2UvyNBTAyqISErzspgxGgyuAoAFU2ljniUQAqbPxsSYyyKc/eQz9npVSTZPYH6qHgwCXGNc0lZh3AHcFmt9gIc9MsvgvgsuAgxp9vaH5967CVj9d9VBk8Y4kZBaASS77ByKASs24ja+AYDEaA6qYGMZi9llQ2lpGYpyRvOztJOTGzd2WsGtQknZkHZVJSu8RslIwk+QXAjWTukO4jM/OvHKdlIUkG3Ej2Azi/VIwBzkE1rmELAGfMrc020FXF+FNDostfGdsvDa9rANVgj/Gn2sS1s3YvgyobK2NQBRgZVG0y1mJ/7xGri0UGz+yIF2SG5CcAXTrlu/0FYD1k8TGAa5GGJdv+nbDhp5EkJP0bTB84XztKzK2RwLnfRoIsuclpzf4A6UyQKIuzXKcAAAAASUVORK5CYII=');
-        newImage.style.display = 'block';
-        newElement.appendChild(newImage);
+    // Assign id and classes to div for logo
+    newElement.classList.add('vjs-logo');
+    newElement.style.float = 'left';
 
-        title = document.querySelector('#' + player.id_ + ' .vjs-dock-text');
-        if (title != null) {
-            title.insertBefore(newElement, title.firstElementChild);
-        }
+    // Assign properties to elements and assign to parents
+    newImage.setAttribute('src', 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAoCAYAAACM/rhtAAACh0lEQVRYCdVYy24UMRCsJiCRIOAESIggEK8LX8ONH+CQMwd+gz/gygV+J4E8pChw4IIiJARIpFBH66Vn1j1Ts0xmg6WVPba7XS5313gH+J8KybdcXXld4+pCq/NJ63nKx+3aYnOAJA3Ao9qkifp2auvMAQLYBLBRmzRRXzeDAB5PBKS2zDcz+1wbiAyuMv4+1MB533kBWD3e8wSwmiBtgKuMwZRBl5bTQvJZ68jL0BsAV8pDUh8AeAngN4CTWe3t8qv1xbkHZvY98Z13k7wqvlje517+beRij7l67LvFD0nX0pvluaM+NrOvHeP9QySfiwy+KN5Ibok2r4pNVx1lpjZvMIMDBD/VvghkLIAfg1P1fZ5KS/DVEOrYX9oKgz8AHBUDkUHP6r1gkzb7GFTY2DMz+gokLwG4l672d+DQzHxjvSUFSNIz8XqvB2CewQDuA+hTBncpxZ9PTAGKR+U+YvwpIeE2UvyNBTAyqISErzspgxGgyuAoAFU2ljniUQAqbPxsSYyyKc/eQz9npVSTZPYH6qHgwCXGNc0lZh3AHcFmt9gIc9MsvgvgsuAgxp9vaH5967CVj9d9VBk8Y4kZBaASS77ByKASs24ja+AYDEaA6qYGMZi9llQ2lpGYpyRvOztJOTGzd2WsGtQknZkHZVJSu8RslIwk+QXAjWTukO4jM/OvHKdlIUkG3Ej2Azi/VIwBzkE1rmELAGfMrc020FXF+FNDostfGdsvDa9rANVgj/Gn2sS1s3YvgyobK2NQBRgZVG0y1mJ/7xGri0UGz+yIF2SG5CcAXTrlu/0FYD1k8TGAa5GGJdv+nbDhp5EkJP0bTB84XztKzK2RwLnfRoIsuclpzf4A6UyQKIuzXKcAAAAASUVORK5CYII=');
+    newImage.style.display = 'block';
+    newElement.appendChild(newImage);
+
+    title = document.querySelector('#' + player.id_ + ' .vjs-dock-text');
+    if (title != null) {
+      title.insertBefore(newElement, title.firstElementChild);
     }
-    
-    videojs.plugin('logo', logoFunc);
-})(videojs);
+  }
+
+  videojs.plugin('logo', logoFunc);
+})(videojs)
--- src/scrollIntoView.js
+++ src/scrollIntoView.js
@@ -1,26 +1,31 @@
 /**
  * Scroll Into View
  */
-(function () {
-    var scrollIntoViewFunc = function () {
-        var player = this;
+(function (videojs) {
+  var scrollIntoViewFunc = function (options) {
+    defaultOptions = {}
+    options = options || {}
+    options = videojs.mergeOptions(defaultOptions, options)
+    console.debug('Plugin scrollIntoView', options)
 
-        var checkIfVideoInView = function () {
-            // Player is fully in viewport, is never played and is in pause
-            if ($(player.el_).isOnScreen(1, 1).inView && !player.hasStarted_ && player.paused()) {
-                player.play()
-            }
-        }
+    var player = this;
 
-        var timer;
-        $(window).on('scroll.playOnScroll', function () {
-            // Trigger 250ms after when finish scrolling and only once
-            clearTimeout(timer)
-            timer = setTimeout(function () {
-                checkIfVideoInView()
-            }, 250)
-        });
+    var checkIfVideoInView = function () {
+      // Player is fully in viewport, is never played and is in pause
+      if ($(player.el_).isOnScreen(1, 1).inView && !player.hasStarted_ && player.paused()) {
+        player.play()
+      }
     }
 
-    videojs.plugin('scrollIntoView', scrollIntoViewFunc);
-}();
+    var timer;
+    $(window).on('scroll.playOnScroll', function () {
+      // Trigger 250ms after when finish scrolling and only once
+      clearTimeout(timer)
+      timer = setTimeout(function () {
+        checkIfVideoInView()
+      }, 250)
+    });
+  }
+
+  videojs.plugin('scrollIntoView', scrollIntoViewFunc);
+})(videojs)

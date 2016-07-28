165082c63bf553d5887e4ce5e223191218bfb7c2
Minh-Quan TRAN <mq.tran@prismamedia.com>
2016-07-28 17:16:28 +0200

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,8 @@
 const gulp = require('gulp');
-const uglify = require('gulp-uglify');
-const pump = require('pump');
 const babel = require('gulp-babel');
 const browserify = require('browserify');
-var transform = require('vinyl-transform');
-var source = require('vinyl-source-stream');
-var fs = require('fs');
-
-gulp.task('compress', (cb) => {
-  pump([
-      gulp.src('src/**/*.js'),
-      uglify(),
-      gulp.dest('dist')
-    ],
-    cb
-  );
-});
-
+const glob = require("glob")
+ 
 gulp.task('babel', (cb) => {
   return gulp.src('src/**/*.js')
     .pipe(babel({
@@ -26,18 +12,16 @@ gulp.task('babel', (cb) => {
 });
 
 gulp.task('browserify', ['babel'], (cb) => {
-  fs.readdir('./build/js', (err, files) => {
-    for(var file of files) {
-      if (file.match(/.+js$/)) {
-        console.log(file)
-        browserify('build/js/' + file)
+  glob("build/**/*.js", function (er, files) {
+      for(var file of files) {
+	console.log(file)
+        browserify(file)
           .bundle()
-          .pipe(source(file))
+          .pipe(source(file.replace('build/js/', '')))
           .pipe(gulp.dest('dist/js'));
       }
-    }
 
-    cb()
+      cb()
   })
 });
 
